97546abe8bdb8f60a9e1adc1979b609288b998b2
nathan-phelipe <146148756+nathan-phelipe@users.noreply.github.com>
2023-10-18 14:14:29 -0300

    Add files via upload

---
+++ aula01/exp04.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php
+
+$color="black";
+var_dump($color);
+$x = 23; 
+$e = 12.3;
+$resultado = $x + $e;
+echo $resultado;  
+
+    ?>
+
+</body>
+</html>
