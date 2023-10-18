e9e18f0996756f9647c34452d26e0c0f51829873
nathan-phelipe <146148756+nathan-phelipe@users.noreply.github.com>
2023-10-18 17:28:45 -0300

    Add files via upload

---
+++ aula02/exp01.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="exp01.css"
+</head>
+<body>
+
+    <pre> seletores </pre>
+    <h1> titulo </h1>
+    <h2> sub titulo  </h2>
+    <p> paragrafo </p>
+
+<pre> seletores com o uso de id = para especificar um unico elemento </pre>
+<p id= "style1"> texto qualquer </p>
+
+<p id= "style1">titulo </p>
+
+<p> paragrafo </p>
+<p> paragrafo </p>
+<p class = "style2">paragrafo</p> 
+<p class = "style2">paragrafo</p> 
+<p class = "style2">usa-se para varios elementos </p> 
+
+</body>
+</html>
---
+++ aula02/exp02.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>exp02</title>
+</head>
+<body>
+    <h1>title</h1>
+
+    <p class="style"> classe do paragrafo </P>
+    
+    <p> de fora </p>
+
+    <div class= "conteudo">
+
+    <p> conteudo da div </P>
+    <p> titulo da div </P>
+    <p id="titulo"> dentro do texto do subconteudo </P>
+    
+</div>
+
+</div>
+</body>
+</html>
